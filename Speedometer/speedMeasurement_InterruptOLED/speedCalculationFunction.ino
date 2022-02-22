15a739ffb4c062b1a3609a11fb1832bb852f6dc3
eddyluursema <50661519+eddyluursema@users.noreply.github.com>
2022-02-22 13:10:16 +0100

    Update speedCalculationFunction.ino

--- Speedometer/speedMeasurement_InterruptOLED/speedCalculationFunction.ino
+++ Speedometer/speedMeasurement_InterruptOLED/speedCalculationFunction.ino
@@ -1,15 +1,11 @@
 /*
   Calculates speed of loc given the time interval passing two IR gateways according to scale and length of gateway
 */
-//const float MILLISPERSECOND = 1000.0;
-//const float MILLIMETERPERMETER = 1000.0;
 const float METERPERSECONDTOKMH = 3.6;
 
 float calculateSpeed(float sensorLength, long intervalMillis, int modelScale) {
   float speed = sensorLength / float(intervalMillis);   // mm per msec
-  //speed  = speed / MILLIMETERPERMETER;                // m per msec
-  //speed  = speed * MILLISPERSECOND;                   // m/s
-  speed  = speed * METERPERSECONDTOKMH;                 // km/h
+   speed  = speed * METERPERSECONDTOKMH;                 // km/h
   speed  = speed * float(modelScale);                   // km/h in real scale
   return speed;
 }
